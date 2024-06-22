629ae1a97b7ed02916d1bc63598b4ce5d61d1eeb
soren essen <sorenessen@sorens-MacBook-Pro.local>
2024-06-22 13:08:56 -0400

    Added semantic ui for menu placement

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
